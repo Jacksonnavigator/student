92da6a2795018b037c40bc8e4cc67f7b90557250
Jacksonnavigator <135830277+Jacksonnavigator@users.noreply.github.com>
2024-12-14 12:47:24 +0300

    Update app.py

--- app.py
+++ app.py
@@ -134,7 +134,6 @@ def teacher_dashboard():
     action = st.radio("Choose Action", ["Upload Results", "View All Results"])
     
     if action == "Upload Results":
-        student_id = st.number_input("Student Id")
         student_name = st.text_input("Student Name")
         subject = st.selectbox("Subject", SUBJECTS)
         marks = st.number_input("Marks", min_value=0, max_value=100, step=1)
